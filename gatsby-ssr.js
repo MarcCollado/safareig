4d313a0643f1cf983fffe2d3680cdd3d72693243
Marc Collado <marc@collado.io>
2020-09-25 17:02:28 +0200

    Inject Layout upon runtime

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,6 @@
 import React from "react";
 import Layout from "./src/components/layout";
 
-import "typeface-montserrat";
-import "typeface-merriweather";
-import "prismjs/themes/prism.css";
-
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
 }
