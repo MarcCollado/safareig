a9c3be012beee49a31b649f058c1db59d6763a19
Marc Collado <marc@collado.io>
2020-09-25 19:30:44 +0200

    Style component

--- src/components/bio.js
+++ src/components/bio.js
@@ -3,7 +3,9 @@ import { useStaticQuery, graphql } from "gatsby";
 import Image from "gatsby-image";
 import styled from "styled-components";
 
+import { CallToAction } from "./styled";
 import { rhythm } from "../utils/typography";
+import ChevronRightCta from "../../content/assets/chevron-right-cta.svg";
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -35,18 +37,43 @@ const Bio = () => {
   const description = data.site.siteMetadata?.description;
   const avatar = data?.avatar?.childImageSharp?.fixed;
 
-  const BioContainer = styled.div``;
-  const Title = styled.h1``;
-  const Description = styled.p``;
-  const ActionContainer = styled.div``;
+  const BioContainer = styled.div`
+    /* Display & Box Model */
+    margin: 1.5rem auto;
+    padding: 0rem 1rem;
+  `;
+  const ActionContainer = styled.div`
+    /* Display & Box Model */
+
+    /* Flex */
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: center;
+    align-self: flex-start;
+
+    p {
+      &:last-of-type {
+        margin-inline-start: 1.5rem;
+      }
+    }
+  `;
+
+  const StyledChevron = styled(ChevronRightCta)`
+    /* Display & Box Model */
+    max-height: 0.75rem;
+    margin-inline-start: 0.5rem;
+  `;
 
   return (
     <BioContainer>
-      <Title>{author?.name}</Title>
-      <Description>{description}</Description>
+      <h1>{author?.name}</h1>
+      <p>{description}</p>
       <ActionContainer>
-        <a>Segueix-nos</a>
-        <a>Fes Safareig</a>
+        <CallToAction>Segueix-nos</CallToAction>
+        <StyledChevron />
+        <CallToAction>Fes Safareig</CallToAction>
+        <StyledChevron />
       </ActionContainer>
     </BioContainer>
   );
