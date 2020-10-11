5ef3e3283558ff2c806cbb9351a9b8d7387b8448
Marc Collado <marc@collado.io>
2020-10-11 18:32:47 +0200

    Use CardEpisode component

--- src/components/episode.js
+++ src/components/episode.js
@@ -1,14 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
-import { CardContainer, CallToAction } from "./styled";
+import { CardEpisode, SimpleLinkContainer, SimpleLink } from "./styled";
 import Chevron from "../../content/assets/chevron-right-cta.svg";
 
 // Styled Components
 
 export const EpisodeDate = styled.p`
   font-weight: 600;
-  letter-spacing: -0.75px;
+  letter-spacing: -1px;
   opacity: 0.5;
   color: var(--black);
   /* Reset default agent margin */
@@ -20,46 +20,23 @@ const EpisodeTitle = styled.h2`
   margin-block-end: 0rem;
 `;
 
-const ActionContainer = styled.div`
-  /* Display & Box Model */
-  margin-bottom: 1.5rem;
-  /* Flex */
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: flex-start;
-  align-items: center;
-  align-self: flex-start;
-  p {
-    margin-inline-start: 1.5rem;
-    padding: 0rem;
-  }
-`;
-
 const StyledChevron = styled(Chevron)`
-  /* Display & Box Model */
-  max-height: 0.75rem;
+  height: 0.75rem;
   margin-inline-start: 0.5rem;
 `;
 
 // Components
 
 const Episode = ({ id, date, episodeNumber, title, description }) => (
-  <CardContainer
-    width={20}
-    flexFlow="column nowrap"
-    justifyContent="flex-start"
-    alignItems="flex-start"
-    plain
-    color="var(--gray)"
-  >
+  <CardEpisode width={17} flexFlow="column nowrap" alignItems="flex-start">
     <EpisodeDate>{date.replace(/-/g, " / ")}</EpisodeDate>
     <EpisodeTitle>{`${episodeNumber}: ${title}`}</EpisodeTitle>
     <p>{description}</p>
-    <ActionContainer>
-      <CallToAction>Escolta Episodi</CallToAction>
+    <SimpleLinkContainer>
+      <SimpleLink>Escolta Episodi</SimpleLink>
       <StyledChevron />
-    </ActionContainer>
-  </CardContainer>
+    </SimpleLinkContainer>
+  </CardEpisode>
 );
 
 export default Episode;
