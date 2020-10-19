9f5f5452b95fedcec33e3a64804fe7f7d6676c4f
Marc Collado <marc@Marc-iMac-215.local>
2020-10-19 15:43:31 +0200

    Add presskit component

---
+++ src/components/press.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+
+import {
+  CardFeatured,
+  CardTitle,
+  SimpleLinkContainer,
+  SimpleLink,
+} from "./styled";
+import PressKitIcon from "../../content/assets/press-kit.svg";
+import Chevron from "../../content/assets/chevron-right-cta.svg";
+
+// Styled Components
+
+const StyledChevron = styled(Chevron)`
+  /* Display & Box Model */
+  max-height: 0.75rem;
+  margin-inline-start: 0.5rem;
+`;
+
+// Components
+
+const Press = () => (
+  <CardFeatured width={17} flexFlow="column nowrap" flat>
+    <CardTitle>
+      <PressKitIcon />
+      <h2>Press Kit</h2>
+    </CardTitle>
+    <p>
+      Hem preparat la descripció de Safareig i un recull d'imatges sobre el
+      podcast perquè et sigui fàcil començar a escriure.
+    </p>
+    <SimpleLinkContainer>
+      <SimpleLink>Descarrega'l</SimpleLink>
+      <StyledChevron />
+    </SimpleLinkContainer>
+  </CardFeatured>
+);
+
+export default Press;
