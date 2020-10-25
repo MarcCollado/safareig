4535a5ccb700c31f958e1979e18d476d6429314d
Marc Collado <marc@collado.io>
2020-10-25 19:00:31 +0100

    Use same picture for mobile and desktop

--- src/components/cover.js
+++ src/components/cover.js
@@ -10,32 +10,22 @@ import { CardContainer } from './styled';
 export const CardCover = styled(CardContainer)`
   width: 20rem;
   padding: 0rem;
+  background-color: var(--blue);
 
   @media (min-width: 576px) {
     width: 22.5rem;
   }
 
   @media (min-width: 768px) {
-    // Turn it into a square
-    width: 18rem;
-    height: 18rem;
+    width: 18rem; // Turn it into a square
+    height: 16.5rem;
+    padding-top: 1.5rem; // Move it dow
   }
 
   @media (min-width: 1024px) {
-    // Turn it into a square
     width: 20rem;
-    height: 20rem;
+    height: 18.5rem;
   }
-`;
-
-const ImageContainer = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
-const StyledImage = styled(Img)`
-  width: 100%;
-  height: 100%;
 `;
 
 // Main components
@@ -61,13 +51,13 @@ const Cover = () => {
     }
   `);
 
-  const coverImage = data.file.childImageSharp.fluid;
-
   return (
     <CardCover>
-      <ImageContainer>
-        <StyledImage alt="Safareig cover art" fluid={coverImage} />
-      </ImageContainer>
+      <Img
+        fluid={data.file.childImageSharp.fluid}
+        alt="Safareig cover image"
+        style={{ width: '100%', height: '100%' }}
+      />
     </CardCover>
   );
 };
