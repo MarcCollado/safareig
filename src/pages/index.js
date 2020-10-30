723b53e39238f2c4469df520c6e25aaf5d1ed20d
Marc Collado <marc@collado.io>
2020-10-30 20:22:06 +0100

    Two column layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,6 +30,33 @@ const HeaderContainer = styled.div`
   }
 `;
 
+const MainContainer = styled.div`
+  @media (min-width: 768px) {
+    margin: 0rem 1rem;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+  }
+`;
+
+const LeftColumn = styled.div``;
+
+const RightColumn = styled.div``;
+
+const MobileEpisodesContainer = styled.div`
+  display: block;
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const DesktopEpisodesContainer = styled.div`
+  display: none;
+  @media (min-width: 768px) {
+    display: block;
+  }
+`;
+
 const IndexPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`;
   const episodes = data.allSanityEpisode?.nodes;
@@ -41,30 +68,60 @@ const IndexPage = ({ data, location }) => {
         <Cover />
         <Bio />
       </HeaderContainer>
-      <Subscribe />
-      <Start />
-      {episodes.map((episode) => {
-        const {
-          id,
-          episodeNumber,
-          title,
-          description,
-          releaseDate: date,
-        } = episode;
-        // const path = episode.path.current;
-        return (
-          <Episode
-            key={id}
-            date={date}
-            episodeNumber={episodeNumber}
-            title={title}
-            description={description}
-          />
-        );
-      })}
-      <Share />
-      <Press />
-      <Footer />
+      <MainContainer>
+        <LeftColumn>
+          <Subscribe />
+          <MobileEpisodesContainer>
+            <Start />
+            {episodes.map((episode) => {
+              const {
+                id,
+                episodeNumber,
+                title,
+                description,
+                releaseDate: date,
+              } = episode;
+              // const path = episode.path.current;
+              return (
+                <Episode
+                  key={id}
+                  date={date}
+                  episodeNumber={episodeNumber}
+                  title={title}
+                  description={description}
+                />
+              );
+            })}
+          </MobileEpisodesContainer>
+          <Share />
+          <Press />
+        </LeftColumn>
+        <RightColumn>
+          <DesktopEpisodesContainer>
+            <Start />
+            {episodes.map((episode) => {
+              const {
+                id,
+                episodeNumber,
+                title,
+                description,
+                releaseDate: date,
+              } = episode;
+              // const path = episode.path.current;
+              return (
+                <Episode
+                  key={id}
+                  date={date}
+                  episodeNumber={episodeNumber}
+                  title={title}
+                  description={description}
+                />
+              );
+            })}
+          </DesktopEpisodesContainer>
+        </RightColumn>
+        <Footer />
+      </MainContainer>
     </>
   );
 };
