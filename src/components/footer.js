6989be0334e16426c8e22a693f0174a514600f01
Marc Collado <marc@collado.io>
2020-10-30 20:21:00 +0100

    Take all the bottom space

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 // Styled components
 
 const FooterContainer = styled.footer`
-  width: 20rem;
+  width: 100%;
   margin: 1rem auto;
   text-align: center;
 `;
