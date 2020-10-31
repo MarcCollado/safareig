a9de0e71151331e068170b1b07342165a14b5ae3
Marc Collado <marc@collado.io>
2020-10-31 16:48:05 +0100

    Animate cover component

--- src/components/cover.js
+++ src/components/cover.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import { CardContainer } from './styled';
 
 // Styled components
 
+const scaleAndRotate = keyframes`
+  0% {
+    transform: scale(1) rotate(-2deg);
+  }
+  100% {
+    transform: scale(1.05) rotate(3deg);
+  }
+`;
+
 export const CardCover = styled(CardContainer)`
   width: 20rem;
   padding: 0rem;
   background-color: var(--blue);
+  transform: scale(1) rotate(-2deg);
+  animation: ${scaleAndRotate} 2s ease-in-out 0s infinite alternate;
 
   @media (min-width: 576px) {
     width: 22.5rem;
@@ -29,20 +40,20 @@ const Cover = () => {
   // GraphQL
   const data = useStaticQuery(graphql`
     query CoverImageQuery {
-      file(relativePath: { eq: "cover-mobile.png" }) {
+      file(relativePath: { eq: "cover-mobile.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 1024) {
             ...GatsbyImageSharpFluid
           }
         }
       }
-      # desktopCover: file(relativePath: { eq: "cover-desktop.png" }) {
-      #   childImageSharp {
-      #     fluid(maxWidth: 1024) {
-      #       ...GatsbyImageSharpFluid
-      #     }
-      #   }
-      # }
+      desktopCover: file(relativePath: { eq: "cover-desktop.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1024) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
     }
   `);
 
