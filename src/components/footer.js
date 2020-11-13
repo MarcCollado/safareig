16125dfe98b6be2ea61d98d40a7c7e34c2d81a43
Marc Collado <marc@collado.io>
2020-11-13 18:54:13 +0100

    Fix inline display

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,16 +9,27 @@ const FooterContainer = styled.footer`
   text-align: center;
 `;
 
+const InLineText = styled.p`
+  display: inline-block;
+  margin: 0.25rem;
+  opacity: 0.3;
+`;
+
+const InLineLink = styled.a`
+  display: inline-block;
+`;
+
 // Main components
 
 const Footer = () => (
   <FooterContainer>
-    <p>
-      Safareig © {new Date().getFullYear()}
-      <br></br>
-      Explora el {` `}
-      <a href="https://github.com/MarcCollado/safareig">codi de la pàgina</a>
-    </p>
+    <InLineText>Safareig © {new Date().getFullYear()}</InLineText>
+    <br />
+    <InLineText>Explora el </InLineText>
+    <InLineLink href="https://github.com/MarcCollado/safareig">
+      codi de la pàgina
+    </InLineLink>
   </FooterContainer>
 );
+
 export default Footer;
