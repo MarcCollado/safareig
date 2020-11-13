51edb4b88ae1d8c84c3b25823f685a7d5adebdd0
Marc Collado <marc@collado.io>
2020-11-13 19:34:02 +0100

    Update card design

--- src/components/episode.js
+++ src/components/episode.js
@@ -1,36 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { CardEpisode, SimpleLinkContainer, SimpleLink } from './styled';
+import {
+  CardEpisode,
+  InnerCardContainer,
+  SimpleLinkContainer,
+  SimpleLink,
+} from './styled';
 import Chevron from '../../content/assets/chevron-right-cta.svg';
 
 // Styled Components
 
 export const EpisodeDate = styled.p`
+  margin: 0rem;
   font-weight: bold;
   letter-spacing: -1px;
   opacity: 0.5;
   color: var(--black);
-  /* Reset default agent margin */
-  margin-block-end: -0.5rem;
+
+  @media (min-width: 768px) {
+    font-size: 14px;
+  }
+
+  @media (min-width: 1024px) {
+    font-size: 18px;
+  }
 `;
 
-const EpisodeTitle = styled.h2`
-  /* Reset default agent margin */
-  margin-block-end: 0rem;
+export const EpisodeTitle = styled.h3`
+  margin-block-start: 1rem;
+  margin-block-end: -0.25rem;
 `;
 
 // Components
 
 const Episode = ({ id, date, episodeNumber, title, description }) => (
   <CardEpisode flexFlow="column nowrap" alignItems="flex-start" flat>
-    <EpisodeDate>{date}</EpisodeDate>
-    <EpisodeTitle>{`${episodeNumber}: ${title}`}</EpisodeTitle>
-    <p>{description}</p>
-    <SimpleLinkContainer>
-      <SimpleLink>Escolta Episodi</SimpleLink>
-      <Chevron />
-    </SimpleLinkContainer>
+    <InnerCardContainer>
+      <EpisodeDate>{date}</EpisodeDate>
+      <EpisodeTitle>{`${episodeNumber}: ${title}`}</EpisodeTitle>
+      <p>{description}</p>
+      <SimpleLinkContainer>
+        <SimpleLink>Escolta Episodi</SimpleLink>
+        <Chevron />
+      </SimpleLinkContainer>
+    </InnerCardContainer>
   </CardEpisode>
 );
 
