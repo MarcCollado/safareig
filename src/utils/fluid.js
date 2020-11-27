8a580e81994f062d0a64ef56ed687aede3712bf1
Marc Collado <marc@collado.io>
2020-11-27 17:50:42 +0100

    fluid util to interpolate width sizes between two points

---
+++ src/utils/fluid.js
@@ -0,0 +1,15 @@
+export const fluid = (i, j) => {
+  const minWidth = 768;
+  const maxWidth = 1440;
+  const widthRatio = minWidth / maxWidth;
+
+  const b = (i - widthRatio * j) / (1 - widthRatio);
+
+  // const match = (i - b) / minWidth === (j - b) / maxWidth;
+  // if (!match) return console.log('Poor math...');
+
+  const m = (i - b) / minWidth;
+  return b > 0
+    ? `clamp(${i}px, calc((100vw * ${m}) + ${b}px), ${j}px)`
+    : `clamp(${i}px, calc((100vw * ${m}) - ${Math.abs(b)}px), ${j}px)`;
+};
