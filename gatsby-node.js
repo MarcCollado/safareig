86881359393f7a38572430029b9df363e3f75130
Marc Collado <marc@collado.io>
2021-01-09 19:58:15 +0100

    Source pages from episodes API

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,22 +3,24 @@ const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions;
-
-  // Define a template for blog post
-  const episode = path.resolve(`./src/components/episode.js`);
-
-  // Get all markdown blog posts sorted by date
+  const episodePage = path.resolve(`./src/pages/episode.js`);
   const result = await graphql(
     `
       {
-        allFeedSafareigFm {
+        allFeedSafareigFm(sort: { order: DESC, fields: isoDate }) {
           nodes {
-            id
             title
             content
             contentSnippet
             isoDate(formatString: "YYYY / MM / DD")
+            itunes {
+              episode
+            }
+            enclosure {
+              url
+            }
           }
+          totalCount
         }
       }
     `
@@ -29,28 +31,31 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return;
   }
 
-  const posts = result.data.allFeedSafareigFm.nodes;
+  const episodes = result.data.allFeedSafareigFm.nodes;
 
-  // Create blog posts pages
-  // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
-  // `context` is available in the template as a prop and as a variable in GraphQL
+  if (episodes.length > 0) {
+    episodes.forEach((e, i) => {
+      const {
+        title,
+        content: descriptionHtml,
+        contentSnippet: description,
+        isoDate: date,
+        itunes: { episode: episodeNumber },
+        enclosure: { url: audioFile },
+      } = e;
+      const previous = i === episodes.length - 1 ? null : episodes[i + 1];
+      const next = i === 0 ? null : episodes[i - 1];
 
-  // if (posts.length > 0) {
-  //   posts.forEach((post, index) => {
-  //     const previous = index === posts.length - 1 ? null : posts[index + 1];
-  //     const next = index === 0 ? null : posts[index - 1];
-
-  //     createPage({
-  //       path: post.fields.slug,
-  //       component: blogPost,
-  //       context: {
-  //         slug: post.fields.slug,
-  //         previous,
-  //         next,
-  //       },
-  //     });
-  //   });
-  // }
+      createPage({
+        path: episodeNumber,
+        component: episodePage,
+        context: {
+          previous,
+          next,
+        },
+      });
+    });
+  }
 };
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
@@ -58,53 +63,10 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode });
-
     createNodeField({
       name: `slug`,
       node,
       value,
     });
   }
-};
-
-exports.createSchemaCustomization = ({ actions }) => {
-  const { createTypes } = actions;
-
-  // Explicitly define the siteMetadata {} object
-  // This way those will always be defined even if removed from gatsby-config.js
-
-  // Also explicitly define the Markdown frontmatter
-  // This way the "MarkdownRemark" queries will return `null` even when no
-  // blog posts are stored inside "content/blog" instead of returning an error
-  createTypes(`
-    type SiteSiteMetadata {
-      author: Author
-      siteUrl: String
-      social: Social
-    }
-
-    type Author {
-      name: String
-      summary: String
-    }
-
-    type Social {
-      twitter: String
-    }
-
-    type MarkdownRemark implements Node {
-      frontmatter: Frontmatter
-      fields: Fields
-    }
-
-    type Frontmatter {
-      title: String
-      description: String
-      date: Date @dateformat
-    }
-
-    type Fields {
-      slug: String
-    }
-  `);
 };
