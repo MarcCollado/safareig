7182b6950d81f3cc430de704b88a609417320834
Marc Collado <marc@collado.io>
2021-01-10 10:51:42 +0100

    Remove handle scroll logic

--- src/components/episode.js
+++ src/components/episode.js
@@ -1,7 +1,5 @@
-import React, { useRef, useState, useEffect } from 'react';
-// import { Link } from 'gatsby';
+import React from 'react';
 import styled from 'styled-components';
-import scrollToElement from 'scroll-to-element';
 
 import { EpisodeCard, InnerCardContainer } from '../styled/cards';
 import { SimpleLinkComposer } from '../styled/link';
@@ -13,8 +11,6 @@ const CardEpisode = styled(EpisodeCard)`
   background-color: ${(props) =>
     props.expand ? 'var(--white)' : 'var(--gray)'};
   box-shadow: ${(props) => (props.expand ? 'var(--boxShadow)' : 'none')};
-  position: ${(props) => (props.expand ? 'relative' : 'static')};
-  z-index: ${(props) => (props.expand ? '999' : '1')};
 
   & div svg:last-child {
     transform: rotate3d(
@@ -27,10 +23,6 @@ const CardEpisode = styled(EpisodeCard)`
 
   @media (min-width: 768px) {
     &:hover {
-      background-color: var(--white);
-      position: relative;
-      z-index: 998;
-
       & div svg:last-child {
         transform: rotate3d(
           0,
@@ -94,68 +86,25 @@ const Episode = ({
   description,
   audioFile,
   showNotes,
-  expandedEpisodeRef,
-  setExpandedEpisodeRef,
 }) => {
-  const [expand, setExpand] = useState(false);
-  const episodeRef = useRef();
-
-  useEffect(() => {
-    const thisEpNum = parseInt(episodeNumber);
-    const expEpRef = expandedEpisodeRef;
-    if (!expand && expEpRef === thisEpNum) {
-      setExpand(true);
-      handleScroll();
-    } else if (expEpRef !== thisEpNum) {
-      setExpand(false);
-    }
-  }, [expandedEpisodeRef, expand]);
-
-  const handleScroll = (duration = 1500, offset = -16) => {
-    const ref = episodeRef.current;
-    return setTimeout(() => {
-      scrollToElement(ref, {
-        offset: offset,
-        // ease options: https://github.com/component/ease
-        ease: 'inOutCube',
-        duration: duration,
-      });
-    }, 10);
-  };
-
-  const handleOnClick = (e, n = episodeNumber) => {
-    e.preventDefault();
-    if (!expand) {
-      setExpandedEpisodeRef(parseInt(n));
-    } else {
-      setExpandedEpisodeRef(parseInt(0));
-    }
-    handleScroll();
-  };
-
   return (
     <CardEpisode
       flexFlow="column nowrap"
       alignItems="flex-start"
-      flat={!expand}
-      expand={expand}
-      ref={episodeRef}
+      flat={false}
+      expand={true}
     >
       <InnerCardContainer>
-        {/* <a href={'/'} onClick={(e) => handleOnClick(e)}> */}
         <EpisodeDate>{date}</EpisodeDate>
         <EpisodeTitle>{`${episodeNumber}: ${title}`}</EpisodeTitle>
         <p>{description}</p>
-        {/* </a> */}
         <Audio
-          hide={!expand}
           controls
           src={audioFile}
           type="audio/mpeg"
           preload="none"
         ></Audio>
         <ShowNotes
-          hide={!expand}
           dangerouslySetInnerHTML={{
             __html: showNotes.replace(
               /href/g,
@@ -163,9 +112,8 @@ const Episode = ({
             ),
           }}
         ></ShowNotes>
-        {/* <a href={'/'} onClick={(e) => handleOnClick(e)}> */}
-        <SimpleLinkComposer text={!expand ? `Escoltar Capítol` : `Tancar`} />
-        {/* </a> */}
+
+        <SimpleLinkComposer text={`Tancar`} />
       </InnerCardContainer>
     </CardEpisode>
   );
