2a96e341da324c72eaa59695ca4c49adf97ae174
Marc Collado <marc@collado.io>
2021-01-10 10:50:49 +0100

    Query additional episode fields

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,4 @@
 const path = require(`path`);
-const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions;
@@ -32,8 +31,10 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   }
 
   const episodes = result.data.allFeedSafareigFm.nodes;
+  const episodesLength = parseInt(episodes.length);
+  const episodesCount = parseInt(result.data.allFeedSafareigFm.totalCount);
 
-  if (episodes.length > 0) {
+  if (episodesLength > 0 && episodesLength === episodesCount) {
     episodes.forEach((e, i) => {
       const {
         title,
@@ -43,30 +44,23 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
         itunes: { episode: episodeNumber },
         enclosure: { url: audioFile },
       } = e;
-      const previous = i === episodes.length - 1 ? null : episodes[i + 1];
+      const previous = i === episodesLength - 1 ? null : episodes[i + 1];
       const next = i === 0 ? null : episodes[i - 1];
 
       createPage({
         path: episodeNumber,
         component: episodePage,
         context: {
+          title,
+          descriptionHtml,
+          description,
+          date,
+          episodeNumber,
+          audioFile,
           previous,
           next,
         },
       });
-    });
-  }
-};
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
-
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode });
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
     });
   }
 };
