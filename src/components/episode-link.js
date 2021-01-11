7d032c288f8689cfaafdcc50b180999c5d3a2166
Marc Collado <marc@collado.io>
2021-01-11 07:41:21 +0100

    EpisodeLink has its own card

--- src/components/episode-link.js
+++ src/components/episode-link.js
@@ -2,45 +2,11 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
 
-import { EpisodeCard, InnerCardContainer } from '../styled/cards';
+import { EpisodeLinkCard, InnerCardContainer } from '../styled/cards';
 import { SimpleLinkComposer } from '../styled/link';
 import { fluid } from '../utils/fluid';
 
 // Styled Components
-
-const CardEpisode = styled(EpisodeCard)`
-  background-color: ${(props) =>
-    props.expand ? 'var(--white)' : 'var(--gray)'};
-  box-shadow: ${(props) => (props.expand ? 'var(--boxShadow)' : 'none')};
-  position: ${(props) => (props.expand ? 'relative' : 'static')};
-  z-index: ${(props) => (props.expand ? '999' : '1')};
-
-  & div svg:last-child {
-    transform: rotate3d(
-      0,
-      0,
-      1,
-      ${(props) => (props.expand ? '-90deg' : '0deg')}
-    );
-  }
-
-  @media (min-width: 768px) {
-    &:hover {
-      background-color: var(--white);
-      position: relative;
-      z-index: 998;
-
-      & div svg:last-child {
-        transform: rotate3d(
-          0,
-          0,
-          1,
-          ${(props) => (props.expand ? '-90deg' : '90deg')}
-        );
-      }
-    }
-  }
-`;
 
 const EpisodeDate = styled.p`
   margin: 0px;
@@ -64,14 +30,14 @@ const EpisodeTitle = styled.h2`
 const EpisodeLink = ({ date, episodeNumber, title, description }) => {
   return (
     <Link to={`/${episodeNumber}`}>
-      <CardEpisode flexFlow="column nowrap" alignItems="flex-start">
+      <EpisodeLinkCard>
         <InnerCardContainer>
           <EpisodeDate>{date}</EpisodeDate>
           <EpisodeTitle>{`${episodeNumber}: ${title}`}</EpisodeTitle>
           <p>{description}</p>
           <SimpleLinkComposer text={`Escoltar Capítol`} />
         </InnerCardContainer>
-      </CardEpisode>
+      </EpisodeLinkCard>
     </Link>
   );
 };
