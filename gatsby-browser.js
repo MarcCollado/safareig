f904c72da428d77342cc4a75a7b42fb0755746cb
Marc Collado <marc@collado.io>
2021-01-27 15:09:12 +0100

    Use prettier

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
-import React from "react";
-import Layout from "./src/components/layout";
+import React from 'react';
+import Layout from './src/components/layout';
 
-import "prismjs/themes/prism.css";
+import 'prismjs/themes/prism.css';
 
 // Wrap all pages with a default Layout
 export function wrapPageElement({ element, props }) {
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Layout from "./src/components/layout";
+import React from 'react';
+import Layout from './src/components/layout';
 
 // Wrap all pages with a default Layout
 export function wrapPageElement({ element, props }) {
