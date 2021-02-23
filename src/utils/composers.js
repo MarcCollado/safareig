5b4ed194aecb4138fb4f8ac5278a0cf254675f23
Marc Collado <marc@collado.io>
2021-02-23 11:49:46 +0100

    Create host composers

---
+++ src/utils/composers.js
@@ -0,0 +1,18 @@
+// id must match its correspondent GraphQL query name
+export const hosts = [
+  {
+    id: 'Marc',
+    name: 'Marc Collado',
+    handle: '@marccollado',
+  },
+  {
+    id: 'Ramon',
+    name: 'Ramon Gilabert',
+    handle: '@ramongilabert',
+  },
+  {
+    id: 'favicon',
+    name: 'Safareig',
+    handle: '@safareigfm',
+  },
+];
