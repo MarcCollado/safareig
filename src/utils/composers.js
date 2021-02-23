74afe696a4c879e4f17de6bbe279cb25c4cac906
Marc Collado <marc@collado.io>
2021-02-23 15:59:31 +0100

    Add podcasts directories

--- src/utils/composers.js
+++ src/utils/composers.js
@@ -11,8 +11,42 @@ export const hosts = [
     handle: '@ramongilabert',
   },
   {
-    id: 'favicon',
+    id: 'Safareig',
     name: 'Safareig',
     handle: '@safareigfm',
+  },
+];
+
+export const podcasts = [
+  {
+    id: 'ApplePodcasts',
+    name: 'Apple Podcasts',
+    url: 'https://podcasts.apple.com/es/podcast/safareig/id1539383682',
+  },
+  {
+    id: 'GooglePodcasts',
+    name: 'Google Podcasts',
+    url:
+      'https://podcasts.google.com/feed/aHR0cHM6Ly9tZWRpYS5yc3MuY29tL3NhZmFyZWlnL2ZlZWQueG1s',
+  },
+  {
+    id: 'Overcast',
+    name: 'Overcast',
+    url: 'https://overcast.fm/+lsTKCnx0s',
+  },
+  {
+    id: 'PocketCasts',
+    name: 'Pocket Casts',
+    url: 'https://pca.st/o70v2mv0',
+  },
+  {
+    id: 'Castro',
+    name: 'Castro',
+    url: 'https://castro.fm/podcast/03163216-961a-430e-9e4d-a0f1b4a91f93',
+  },
+  {
+    id: 'RSS',
+    name: 'RSS Feed',
+    url: 'https://media.rss.com/safareig/feed.xml',
   },
 ];
