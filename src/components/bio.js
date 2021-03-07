66fafc937baaab476ae40f7abbdb7fd17a93c110
Marc Collado <marc@collado.io>
2021-03-07 16:26:18 +0100

    Export description from text

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,41 +1,16 @@
 import React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
-import styled from 'styled-components';
 
 import {
   FeaturedLinkComposer,
   InLineLinksContainer,
   PillLinkComposer,
 } from '../styled/link';
+import { HeaderDescription } from '../styled/text';
 import { BioContainer } from '../utils/containers';
 import { getRandom } from '../utils/random';
 
-// Styled components
-
-const Description = styled.p`
-  // correct for <p> default block margin
-  margin-block-start: 12px;
-  margin-block-end: 24px;
-  font-size: 17px;
-
-  @media (min-width: 576px) {
-    font-size: 20px;
-  }
-
-  @media (min-width: 768px) {
-    font-size: 22px;
-  }
-
-  @media (min-width: 1024px) {
-    font-size: 24px;
-  }
-
-  @media (min-width: 1280px) {
-    font-size: 28px;
-  }
-`;
-
-// Main components
+// Main Components
 
 const Bio = () => {
   // GraphQL
@@ -64,11 +39,11 @@ const Bio = () => {
   return (
     <BioContainer>
       <h1>{header}</h1>
-      <Description>
+      <HeaderDescription>
         El teu aperitiu setmanal on analitzem com la tecnologia està canviant la
         societat que ens envolta. Cada dilluns, en 20 minuts, i exclusivament
         en&nbsp;català.
-      </Description>
+      </HeaderDescription>
       <InLineLinksContainer>
         <Link to={`/${randomEspisode}`}>
           <PillLinkComposer text="Escolta'ns" />
