eb2bcebed8bc6dcaf5a07280804c6f36c25b3a62
Marc Collado <marc@collado.io>
2021-03-07 17:47:13 +0100

    Add post link component

---
+++ src/components/post-link.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+import { CardTitle, InnerCardContainer, PostLinkCard } from '../styled/cards';
+import { Meta } from '../styled/text';
+import { fluid } from '../utils/fluid';
+import PostIcon from '../../content/assets/press-kit.svg';
+
+// Styled Components
+
+const InlineItems = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (min-width: 768px) {
+    margin-block-start: ${fluid(32, 48)};
+  }
+`;
+
+const PostMeta = styled(Meta)`
+  font-weight: 500;
+  letter-spacing: 0px;
+`;
+
+// Main Component
+
+const PostLink = ({
+  authorName,
+  date,
+  excerpt,
+  path,
+  primaryTag,
+  readingTime,
+  title,
+}) => {
+  return (
+    <Link to={`/bugada/${path}`}>
+      <PostLinkCard>
+        <InnerCardContainer>
+          <CardTitle>
+            <PostIcon />
+            <h2>{title}</h2>
+          </CardTitle>
+          <p>{excerpt}</p>
+          <InlineItems>
+            <PostMeta>{authorName}</PostMeta>
+            <PostMeta>
+              {`${date} • ${readingTime} min • ${primaryTag}`}
+            </PostMeta>
+          </InlineItems>
+        </InnerCardContainer>
+      </PostLinkCard>
+    </Link>
+  );
+};
+
+export default PostLink;
